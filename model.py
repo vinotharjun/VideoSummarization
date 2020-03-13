db46d26bbbdb4fc5d82228a95228c5bf74d8d0d0
Vinod Arjun <32776658+vinotharjun@users.noreply.github.com>
2020-03-13 15:14:56 +0530

    updated the weight path of vgg16

--- model.py
+++ model.py
@@ -9,11 +9,12 @@ from keras.models import Sequential
 from keras.layers import LSTM,Dense,TimeDistributed,Bidirectional,Input,Conv3D,AveragePooling3D,GlobalAveragePooling2D,Flatten,GlobalAvgPool3D,GlobalAveragePooling2D,GlobalAveragePooling3D
 from env_variable import *
 def get_vgg_model():
-    model_feat = VGG16(weights=None, include_top=False)
+#     model_feat = VGG16(weights=None, include_top=False)
+    model_feat = VGG16(weights="imagenet", include_top=False)
     gap = GlobalAveragePooling2D(data_format="channels_last")(model_feat.output)
     feat_extractor=Model(inputs=model_feat.input,output=gap)
     #loading the weights from saved model
-    feat_extractor.load_weights("./saved model/model.h5")
+#     feat_extractor.load_weights("./saved model/model.h5")
     return feat_extractor 
 
 def get_model(maxlen=maxlen,mode="train"):
