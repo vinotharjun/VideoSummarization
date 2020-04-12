6e1ba89c3b5c8d0c3640310e091b310b23652c8e
Vinod Arjun <32776658+vinotharjun@users.noreply.github.com>
2020-04-12 19:50:52 +0530

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,43 @@
+import cv2
+from env_variable import maxlen
+import pandas as pd
+import numpy as np
+from glob import glob
+import os
+import re
+# !pip install scikit-video
+# !apt-get install --no-install-recommends ffmpeg
+import skvideo.io  
+from skimage.transform import resize
+import skimage
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+import keras
+from keras.models import Model, Input
+from keras.applications.vgg16 import preprocess_input,VGG16
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import LSTM,Dense,TimeDistributed,Bidirectional,Input,Conv3D,AveragePooling3D,GlobalAveragePooling2D,Flatten,GlobalAvgPool3D,GlobalAveragePooling2D,GlobalAveragePooling3D
+from env_variable import *
+
+path1 = ""
+path2 = ""
+
+def extract_feature(data,feat_extractor):
+    # print(data.shape)
+    x = preprocess_input(data)
+    features = feat_extractor.predict(x)
+    print(features.shape)
+    return features
+    
+def resize_image(data,size=224):
+  X_data_resized = [ cv2.resize(image, (size,size)) for image in data]
+  x = np.asarray(X_data_resized)
+  return x
+
+def read_image(path1,path2):
+``x1 = cv2.resize(np.array(PIL.open(path1).convert("RGB")),(224,224))
+  x2 = cv2.resize(np.array(PIL.open(path2).convert("RGB")),(224,224))
+  return x1,x2
+  
+  
